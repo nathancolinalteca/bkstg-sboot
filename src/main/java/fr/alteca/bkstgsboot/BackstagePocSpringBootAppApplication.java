4a46d2027dba39a0016de619d22e02bf2735d06c
Nathan COLIN <ncolin@alteca.fr>
2022-12-05 09:09:03 +0100

    init

---
+++ src/main/java/fr/alteca/bkstgsboot/BackstagePocSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package fr.alteca.bkstgsboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackstagePocSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackstagePocSpringBootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/alteca/bkstgsboot/BackstagePocSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.alteca.bkstgsboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackstagePocSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
